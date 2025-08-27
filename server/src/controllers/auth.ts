db1fb8a10dcd7f2b59e822265f6e67b97d5a3391
vinay-krishna9 <vinay.krishnaprasad96@gmail.com>
2025-08-28 01:47:13 +0200

    feat: register + login user + db connection

---
+++ server/src/controllers/auth.ts
@@ -0,0 +1,95 @@
+import { Request, Response } from "express";
+import prisma from "../database/db";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export const registerUser = async (req: Request, res: Response) => {
+  try {
+    console.log("🚀 Registering User ~", req.body);
+
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "Invalid input" });
+    }
+
+    // Check if user already exists
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // Password hashing
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    // Create user
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: passwordHash,
+      },
+      select: {
+        id: true,
+        email: true,
+      },
+    });
+
+    // Generate JWT token
+    if (!process.env.JWT_SECRET) {
+      throw new Error("JWT_SECRET environment variable is not defined");
+    }
+
+    const token = jwt.sign(
+      { userId: user.id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.status(201).json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+export const loginUser = async (req: Request, res: Response) => {
+  try {
+    console.log("🚀 Loggin In User ~", req.body);
+
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "Invalid input" });
+    }
+
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    if (!process.env.JWT_SECRET) {
+      throw new Error("JWT_SECRET environment variable is not defined");
+    }
+
+    const token = jwt.sign(
+      { userId: user.id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,24 +2,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
-import pool from "./database/db";
+import authRoute from "./routes/auth";
 
 const app = express();
+app.use(express.json());
 const port = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("Hello from TypeScript + Express!");
-});
-
-app.get("/db-check", async (req, res) => {
-  try {
-    const result = await pool.query("SELECT 1");
-    res.json({ success: true, result: result.rows });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ success: false, error: err });
-  }
-});
+app.use("/auth", authRoute);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
