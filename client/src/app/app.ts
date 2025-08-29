fa24799949edd49776b7a3a013ba6dcdf54e62ba
vinay-krishna9 <vinay.krishnaprasad96@gmail.com>
2025-08-29 22:19:54 +0200

    chore: componets added + bootstrap initialization

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Registration } from './components/registration/registration';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Registration],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('client');
-}
+export class App {}
---
+++ client/src/app/components/registration/registration.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registration',
+  imports: [],
+  templateUrl: './registration.html',
+  styleUrl: './registration.scss'
+})
+export class Registration {
+
+}
