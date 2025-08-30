bc413b4a17668b598812802953d7e70aed5723ff
vinay-krishna9 <vinay.krishnaprasad96@gmail.com>
2025-08-30 14:53:18 +0200

    feat: cors added

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,13 +2,21 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import cors from "cors";
 
 import authRoute from "./routes/auth";
 import userRoute from "./routes/user";
 
+const port = process.env.PORT;
 const app = express();
 app.use(express.json());
-const port = process.env.PORT;
+app.use(
+  cors({
+    origin: "http://localhost:4200",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use("/auth", authRoute);
 app.use("/api", userRoute);
